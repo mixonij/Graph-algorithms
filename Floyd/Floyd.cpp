6cafa1e60a3da69d9c00efbddb9d97da6a71f411
mixonij <mixon1000@mail.ru>
2019-02-07 17:34:37 +0300

    Initial commit

--- Floyd/Floyd.cpp
+++ Floyd/Floyd.cpp
@@ -1,21 +1,70 @@
-﻿// Floyd.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+#include <list>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include <algorithm>
+
+void FU(int **D, int V)
+{
+	double tn = clock();
+	int k;
+
+	for (size_t k = 0; k < V; k++)
+		for (size_t i = 0; i < V; i++)
+			for (size_t j = 0; j < V; j++)
+				if (D[i][k] && D[k][j] && i != j)
+					if (D[i][k] + D[k][j] < D[i][j] || D[i][j] == 0)
+						D[i][j] = D[i][k] + D[k][j];
+
+	double tk = clock();
+
+	std::string name = "out_matr_" + std::to_string(V) + ".txt";
+	std::ofstream matrF(name);
+
+	for (size_t i = 0; i < V; i++)
+	{
+		for (size_t j = 0; j < V; j++) matrF << D[i][j] << " ";
+		matrF << std::endl;
+	}
+	matrF << std::endl << "Time: " << (tk - tn) / 1000.0;
+	matrF.close();
+}
+
+void generate(int n, int **mas) {
+	srand(time(NULL));
+	std::string name = "matr_" + std::to_string(n) + ".txt";
+	std::ofstream matrF(name);
+	for (size_t i = 0; i < n; i++) {
+		for (size_t j = 0; j < n; j++) {
+			if (i == j) {
+				mas[i][j] = 0;
+				matrF << 0 << " ";
+			}
+			else {
+				int t = rand() % 50;
+				mas[i][j] = t;
+				matrF << t << " ";
+			}
+		}
+		matrF << std::endl;
+	}
+	matrF.close();
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::list<int> n{ 3, 100, 250, 500 , 1000 };
+	for (int num : n) {
+		int **mas = new int *[num];
+		for (size_t i = 0; i < num; i++)
+			mas[i] = new int[num];
+		generate(num, mas);
+		FU(mas, num);
+		for (int i = 0; i < num; i++)
+			delete[]mas[i];
+		delete[]mas;
+	}
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
